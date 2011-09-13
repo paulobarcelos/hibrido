4089b09c54c1eb16011e153dc82f682e68de31fc
Paulo Barcelos <paulo@azucrina.org>
2011-09-14 01:11:13 +0200

    More on the hover...

--- footer.php
+++ footer.php
@@ -8,6 +8,8 @@
 
 
   	<!-- scripts concatenated and minified via build script -->
+  	<script defer src="<?php bloginfo('stylesheet_directory');?>/js/mylibs/jquery.color.js"></script>
+  	<script defer src="<?php bloginfo('stylesheet_directory');?>/js/mylibs/jquery.transform.light.js"></script>
   	<script defer src="<?php bloginfo('stylesheet_directory');?>/js/mylibs/RequestAnimationFrame.js"></script>
   	<script defer src="<?php bloginfo('stylesheet_directory');?>/js/plugins.js"></script>
   	<script defer src="<?php bloginfo('stylesheet_directory');?>/js/script.js"></script>
--- js/script.js
+++ js/script.js
@@ -9,32 +9,63 @@ HIBRIDO.site = new function(){
 	var shapes = [];
 	this.init = function(){		
 		this.animate();
-		$('.shapes').each(function(index) {
-			console.log
-			$(this).children().each(function(index) {
-				var shape = [$(this), (Math.random()*1-0.5), 0];
+		$('#mainnavigation').find('li').each(function(index) {
+			_this.deselect($(this), true);
+			$(this).children('.shapes').children().each(function(index) {
+				var shape = [$(this), (Math.random()*2-1), 0];
 				shapes.push(shape);
 			});
-		});
+			
+			$(this).hover(function(){
+				_this.select($(this))
+			},function () {
+				_this.deselect($(this))
+			});
+		});		
+	}
+	
+	this.deselect = function(item, imediate){
+		imediate = imediate || false;
+		var duration = (imediate)?0:300;
+		var scaleString = 'scale('+0.6+')';
+		item.children('.shapes').stop(true).animate({
+		    transform : scaleString
+		}, duration+duration);
+		
+		item.children('.link').stop(true).animate({
+			'color' : '#444',
+		    top : (232)		    
+		}, duration);
+	}
+	
+	this.select = function(item, imediate){
+		imediate = imediate || false;
+		var duration = (imediate)?0:300;
+		var scaleString = 'scale('+1+')';
+		item.children('.shapes').stop(true).animate({
+		    transform : scaleString
+		}, duration+duration);
+		
+		item.children('.link').stop(true).animate({
+			'color' : '#fff',
+		    top : (232/2)	    
+		}, duration);
 	}
 	
 	this.animate = function(){
 		requestAnimationFrame(_this.animate);
-		for(var i = 0; i < shapes.length; i++){
-			_this.rotate(shapes[i]);
-		}
+		_this.render();
 	}
-	this.rotate = function(shape){
-		var newAngle = shape[2]+shape[1];
-		shape[2] = newAngle;
-		var angleString = 'rotate('+newAngle+'deg)';
-		shape[0].css({
-		    '-webkit-transform': angleString,
-		    '-moz-transform': angleString,
-		    '-o-transform': angleString,
-		    '-ms-transform': angleString,
-		    transform : angleString
-		});
+	this.render = function(){
+		for(var i = 0; i < shapes.length; i++){
+			var shape = shapes[i];
+			var newAngle = shape[2]+shape[1];
+			shape[2] = newAngle;
+			var angleString = 'rotate('+newAngle+'deg)';
+			shape[0].css({
+			    transform : angleString
+			});
+		}		
 	}
 }
 
